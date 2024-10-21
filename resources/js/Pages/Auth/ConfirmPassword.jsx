877265153ab22bef6fb2643ccc365c0cdef8bca6
Gilbike <marton.dikacz@gmail.com>
2024-10-21 23:28:06 +0200

    feat: :lipstick: reshape confirm password page

--- resources/js/Pages/Auth/ConfirmPassword.jsx
+++ resources/js/Pages/Auth/ConfirmPassword.jsx
@@ -1,55 +1,45 @@
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, useForm } from '@inertiajs/react';
+import Button from "@/Components/Button";
+import FormInput from "@/Components/FormInput";
+import { Head, useForm } from "@inertiajs/react";
 
 export default function ConfirmPassword() {
     const { data, setData, post, processing, errors, reset } = useForm({
-        password: '',
+        password: "",
     });
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('password.confirm'), {
-            onFinish: () => reset('password'),
+        post(route("password.confirm"), {
+            onFinish: () => reset("password"),
         });
     };
 
     return (
-        <GuestLayout>
+        <div className="flex min-h-screen flex-col items-center bg-neutral-100 justify-center">
             <Head title="Confirm Password" />
 
-            <div className="mb-4 text-sm text-gray-600">
-                This is a secure area of the application. Please confirm your
-                password before continuing.
-            </div>
+            <div className="w-11/12 md:w-5/12 xl:w-3/12 p-6 bg-white rounded-lg drop-shadow">
+                <div className="mb-4 text-sm text-gray-600">
+                    This is a secure area of the application. Please confirm
+                    your password before continuing.
+                </div>
 
-            <form onSubmit={submit}>
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
+                <form onSubmit={submit}>
+                    <FormInput
+                        fieldName="password"
                         type="password"
-                        name="password"
                         value={data.password}
-                        className="mt-1 block w-full"
-                        isFocused={true}
-                        onChange={(e) => setData('password', e.target.value)}
+                        onChange={(e) => setData("password", e.target.value)}
+                        label="Password"
+                        error={errors.password}
                     />
 
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4 flex items-center justify-end">
-                    <PrimaryButton className="ms-4" disabled={processing}>
+                    <Button className="mt-6 w-full" disabled={processing}>
                         Confirm
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+                    </Button>
+                </form>
+            </div>
+        </div>
     );
 }
