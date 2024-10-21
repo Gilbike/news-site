5d8487316e1c19cd3690cec0b94c001dd55f9295
Gilbike <marton.dikacz@gmail.com>
2024-10-21 20:25:12 +0200

    feat: :card_file_box: seed admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@news.com',
+            'password' => bcrypt('admin'),
+            'firstname' => 'Journalist',
+            'lastname' => 'Name'
         ]);
     }
 }
