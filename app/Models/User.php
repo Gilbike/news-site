ac50349f3fe87f28ad806e6f515d4edee1e09c7b
Gilbike <marton.dikacz@gmail.com>
2024-10-21 20:22:43 +0200

    feat: :card_file_box: alter users table to add journalist info

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'firstname',
+        'lastname'
     ];
 
     /**
@@ -43,6 +45,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'editor' => 'boolean'
         ];
     }
 }
