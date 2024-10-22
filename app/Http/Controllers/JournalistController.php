13a55fd5826d9638be9b1318a985e51bea1865d4
Gilbike <marton.dikacz@gmail.com>
2024-10-22 22:42:19 +0200

    feat: :lock: add editor middleware for editor duties

--- app/Http/Controllers/JournalistController.php
+++ app/Http/Controllers/JournalistController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class JournalistController extends Controller
+class JournalistController extends Controller implements HasMiddleware
 {
+    public static function middleware()
+    {
+        return [
+            new Middleware('editor', only: ['create']),
+        ];
+    }
+
     /**
      * Display a listing of the resource.
      */
