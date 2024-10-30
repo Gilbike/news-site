16f89bba2ad83f5d36325f35b00b2ea3c3a8da64
Gilbike <marton.dikacz@gmail.com>
2024-10-30 12:35:32 +0100

    fix: :lipstick: fix profile dropdown width

--- resources/js/Components/DashboardNavbar.jsx
+++ resources/js/Components/DashboardNavbar.jsx
@@ -51,7 +51,7 @@ export default function DashboardNavbar() {
                         <Link
                             href="/logout"
                             method="post"
-                            className="flex px-3 py-2 hover:bg-neutral-100"
+                            className="flex w-full px-3 py-2 hover:bg-neutral-100"
                             as="button"
                         >
                             Logout
