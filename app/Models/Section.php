73a3d52610a49bbdd6124dfc65e7ce99ad19a0e0
Gilbike <marton.dikacz@gmail.com>
2024-11-03 01:06:05 +0100

    feat: :card_file_box: add section has many relation to articles

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Section extends Model
 {
@@ -11,4 +12,9 @@ class Section extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
 }
