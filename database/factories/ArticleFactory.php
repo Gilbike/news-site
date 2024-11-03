63c0694b3995d293b144ac78b5f79268173cca2f
Gilbike <marton.dikacz@gmail.com>
2024-11-03 01:05:10 +0100

    refactor: :card_file_box: refactored seeder for articles

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->realText(50),
+            'small_summary' => fake()->realText(300),
+            'large_summary' => fake()->boolean() ? fake()->realText(1000) : null,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Section;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,21 +16,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $sections = Section::factory(5)->create();
 
-        User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@news.com',
-            'password' => bcrypt('admin'),
-            'firstname' => 'Journalist',
-            'lastname' => 'Name',
-            'editor' => true,
-        ]);
+        User::factory()
+            ->create([
+                'name' => 'admin',
+                'email' => 'admin@news.com',
+                'password' => bcrypt('admin'),
+                'firstname' => 'Journalist',
+                'lastname' => 'Name',
+                'editor' => true,
+            ]);
 
-        User::factory(5)->create();
-
-        $this->call([
-            SectionSeeder::class
-        ]);
+        User::factory(5)
+            ->has(
+                Article::factory()
+                    ->count(10)
+                    ->state(new Sequence(
+                        fn(Sequence $sequence) => ['section_id' => $sections->random()],
+                    ))
+            )
+            ->create();
     }
 }
