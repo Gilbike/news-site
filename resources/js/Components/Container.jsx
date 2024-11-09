1d1bc774d188300a621a595f62393209e89c5293
Gilbike <marton.dikacz@gmail.com>
2024-11-09 22:43:44 +0100

    fix: :lipstick: add small padding for better mobile ux

--- resources/js/Components/Container.jsx
+++ resources/js/Components/Container.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function Container({ children }) {
     return (
-        <div className="m-auto w-full sm:w-10/12 md:w-7/12 ">{children}</div>
+        <div className="m-auto w-full sm:w-10/12 md:w-7/12 p-2 sm:p-0">
+            {children}
+        </div>
     );
 }
